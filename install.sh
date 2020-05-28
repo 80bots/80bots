fc3c522e671bca9c9fc9969310b1ef86bd90a011
romfis <roman.bikbaiev.dev@gmail.com>
2020-05-28 13:32:40 +0300

    Exit if Git User' Name and Email are not configured

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+GIT_NAME="$(git config user.name)"
+GIT_EMAIL="$(git config user.email)"
+
+if [[ -z $GIT_NAME || -z $GIT_EMAIL ]]
+ then
+  echo "Please configure git user name and email" && exit -1
+fi
 
 git submodule init
 git submodule update
