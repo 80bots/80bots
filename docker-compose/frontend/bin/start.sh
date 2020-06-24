6da5be6ed5807dcec48f18df68c1c7db0fad22d4
romfis <roman.bikbaiev.dev@gmail.com>
2020-06-24 16:51:47 +0300

    Rename variables and containers for builder

--- docker-compose/frontend/bin/start.sh
+++ docker-compose/frontend/bin/start.sh
@@ -8,10 +8,6 @@ echo "NODE_PATH=$NODE_PATH" >> ./.env
 echo "PORT=$PORT" >> ./.env
 echo "API_URL=$API_URL" >> ./.env
 echo "SOCKET_URL=$SOCKET_URL" >> ./.env
-echo "STRIPE_PUBLIC_KEY=$STRIPE_PUBLIC_KEY" >> ./.env
-echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID" >> ./.env
-echo "FACEBOOK_CLIENT_ID=$FACEBOOK_CLIENT_ID" >> ./.env
-echo "SENTRY_DSN=$SENTRY_DSN" >> ./.env
 
 if [[ $APP_ENV == 'production' ]]; then
   yarn build && yarn start
