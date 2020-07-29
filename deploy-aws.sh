f633f01644226829a30395d88eb5b97e49e6f002
romfis <roman.bikbaiev.dev@gmail.com>
2020-07-29 18:10:43 +0300

    Updated deploy-aws script

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -13,6 +13,19 @@ function checkYesOrNo() {
   done
 }
 
+# Function check if the line is empty by the user.
+function checkDefault() {
+  while read -r -p "$1" DATA; do
+    DATA=${DATA}
+    if [[ "$DATA" =~ ^()$ ]]; then
+      continue
+    else
+      echo "$DATA"
+      break
+    fi
+  done
+}
+
 docker run --rm -it amazon/aws-cli --version
 alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'
 shopt -s expand_aliases
@@ -20,8 +33,14 @@ shopt -s expand_aliases
 ANSWER=$(checkYesOrNo "You have AWS CLI configurations configured? [y/N]: ")
 
 if [[ "$ANSWER" =~ ^(yes|y)$ ]]; then
- aws cloudformation create-stack --stack-name test-80bots --template-body file://80bots-template.yaml --parameters ParameterKey=KeyName,ParameterValue=keyCloudFormation
+  KEY_CLOUD_FORMATION=$(checkDefault "KeyPair name
+  > for example: $(tput setaf 2)keyCloudFormation$(tput sgr0)
+  > $(tput setaf 1)(required)$(tput sgr0): ")
+ aws cloudformation create-stack --stack-name stack-80bots --template-body file://80bots-template.yaml --parameters ParameterKey=KeyName,ParameterValue=${KEY_CLOUD_FORMATION}
 elif [[ "$ANSWER" =~ ^(no|n)$ ]]; then
  aws configure
- aws cloudformation create-stack --template-body ./80bots-template.yaml --stack-name test-80bots --parameters ParameterKey=KeyName, ParameterValue=keyCloudFormation ParameterKey=InstanceType
+  KEY_CLOUD_FORMATION=$(checkDefault "KeyPair name
+  > for example: $(tput setaf 2)keyCloudFormation$(tput sgr0)
+  > $(tput setaf 1)(required)$(tput sgr0): ")
+ aws cloudformation create-stack --stack-name stack-80bots --template-body file://80bots-template.yaml --parameters ParameterKey=KeyName,ParameterValue=${KEY_CLOUD_FORMATION}
 fi
